f2e523e810552f6668078199aa1591e39d925426
pikimaru <pikajunglist@gmail.com>
2022-06-21 14:01:24 +0200

    Day 2, WIP

--- script.js
+++ script.js
@@ -41,24 +41,73 @@ let autoGenerateBoard = (function(){
         let NewDiv = document.createElement('div')
         board1.appendChild(NewDiv).setAttribute('id',i)
         board1.appendChild(NewDiv).setAttribute('class','free')
-        if (i%2==0){
-            NewDiv.innerHTML= 'x'
-        }else{
-            NewDiv.innerHTML= 'o'
-        }
+        // if (i%2==0){
+        //     NewDiv.innerHTML= 'x'
+        // }else{
+        //     NewDiv.innerHTML= 'o'
+        // }
     }
 })();
 
-let putX = function(){
+let putX = function(event){
     let freeSpots = document.querySelectorAll('.free')
-    console.log('start')
-    for (let i = 0; i>0; i++){
-        console.log("i")} 
+    console.log(event)
 
 }
 
 
 
-   putX()
+// let id1 = document.getElementById("1").addEventListener('click',function(){
+//     let id1o = document.getElementById('1').innerHTML = "X"
+// })
+// let id2 = document.getElementById("2").addEventListener('click',function(){
+//     let id1o = document.getElementById('2').innerHTML = "X"
+//     putX()
+// })
 
-   
+let globalTurn = 0;
+let XO = document.getElementsByClassName('XO')
+let XOChild = document.querySelectorAll('.XOChild');
+document.querySelector('body').addEventListener('click',function(event){
+
+    if (event.target.classList[0] == 'free'){
+        console.log(event.target.classList)
+       
+        if (globalTurn % 2 == 0){
+        let addClass = event.target.classList.add('taken')
+        let rmClass = event.target.classList.remove('free')
+        let putX = event.target.innerHTML = "O"
+        } else{
+            let addClass = event.target.classList.add('taken')
+            let rmClass = event.target.classList.remove('free')
+            let putX = event.target.innerHTML = "X"
+        }
+    }
+
+})
+
+document.addEventListener('keydown',function(event){
+
+    if (event.key == 'Shift'){
+    
+
+        if (globalTurn % 2 == 0){
+            console.log("its X turn")
+            XOChild[0].setAttribute('id','turn')
+            XOChild[1].setAttribute('id','')
+            globalTurn++
+            console.log(globalTurn)
+        } else {
+            console.log("its O turn")
+            XOChild[0].setAttribute('id','')
+            XOChild[1].setAttribute('id','turn')
+            globalTurn++
+            console.log(globalTurn)
+        }
+    }
+
+})
+    
+
+
+
