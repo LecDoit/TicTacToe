f3334f5c7e6138e31e12a6392250bd5068c4f1a0
pikimaru <pikajunglist@gmail.com>
2022-06-21 21:06:38 +0200

    Day 2 of Development - done

--- script.js
+++ script.js
@@ -15,23 +15,9 @@ let playerFactory = function(name, position,symbol){
 let pika =  new player('pika',"first","cross")
 let pikaF =  playerFactory('pika',"first","cross")
 
-let calc = (function(){
-    let add = function(a,b){a+b}
- return (add)})();
 
- let abc = 'hard';
- let one = 1;
- let two = 2;
  
 
- let sex = (function(){
-    let gigi = "badman ting"
-
-    let aman = function(){
-        console.log('this method gonna print stuff')}
-    return {
-        gigi}
-    }());
 
 
 let autoGenerateBoard = (function(){
@@ -41,11 +27,6 @@ let autoGenerateBoard = (function(){
         let NewDiv = document.createElement('div')
         board1.appendChild(NewDiv).setAttribute('id',i)
         board1.appendChild(NewDiv).setAttribute('class','free')
-        // if (i%2==0){
-        //     NewDiv.innerHTML= 'x'
-        // }else{
-        //     NewDiv.innerHTML= 'o'
-        // }
     }
 })();
 
@@ -57,35 +38,41 @@ let putX = function(event){
 
 
 
-// let id1 = document.getElementById("1").addEventListener('click',function(){
-//     let id1o = document.getElementById('1').innerHTML = "X"
-// })
-// let id2 = document.getElementById("2").addEventListener('click',function(){
-//     let id1o = document.getElementById('2').innerHTML = "X"
-//     putX()
-// })
-
 let globalTurn = 0;
+let roundCount = true;
+
+
+//3
 let XO = document.getElementsByClassName('XO')
 let XOChild = document.querySelectorAll('.XOChild');
 document.querySelector('body').addEventListener('click',function(event){
+    
+    if (roundCount == true){
 
     if (event.target.classList[0] == 'free'){
-        console.log(event.target.classList)
+       
        
         if (globalTurn % 2 == 0){
         let addClass = event.target.classList.add('taken')
         let rmClass = event.target.classList.remove('free')
         let putX = event.target.innerHTML = "O"
-        } else{
-            let addClass = event.target.classList.add('taken')
-            let rmClass = event.target.classList.remove('free')
-            let putX = event.target.innerHTML = "X"
-        }
-    }
+        roundCount = false
+        winning()
 
+        } else{
+        let addClass = event.target.classList.add('taken')
+        let rmClass = event.target.classList.remove('free')
+        let putX = event.target.innerHTML = "X"
+        roundCount = false
+        winning()
+        }
+    }console.log('field has been chosen and roundCount is ' + roundCount)
+    } else {
+    alert("Please pass it to another player with shift")
+    }
 })
 
+//4
 document.addEventListener('keydown',function(event){
 
     if (event.key == 'Shift'){
@@ -96,18 +83,136 @@ document.addEventListener('keydown',function(event){
             XOChild[0].setAttribute('id','turn')
             XOChild[1].setAttribute('id','')
             globalTurn++
-            console.log(globalTurn)
+            roundCount = true
+
         } else {
             console.log("its O turn")
             XOChild[0].setAttribute('id','')
             XOChild[1].setAttribute('id','turn')
             globalTurn++
-            console.log(globalTurn)
+            roundCount = true
+
         }
-    }
+    }console.log('passing to next player ' + roundCount)
+
 
 })
     
 
+//5 winning code
+//winning scenarios
+// horizontal 123 456 789
+// vertical 963 852 741  
+// diagonal 951 753
+
+let winning = function(){
+    let cel1 = document.getElementById('1')
+    let cel2 = document.getElementById('2')
+    let cel3 = document.getElementById('3')
+    let cel4 = document.getElementById('4')
+    let cel5 = document.getElementById('5')
+    let cel6 = document.getElementById('6')
+    let cel7 = document.getElementById('7')
+    let cel8 = document.getElementById('8')
+    let cel9 = document.getElementById('9')
+
+ 
+
+    if (cel1.textContent =='X' && cel2.textContent=='X' && cel3.textContent=='X'){
+        alert('X won!')
+        cel1.classList.add('win')
+        cel2.classList.add('win')
+        cel3.classList.add('win')
+        
+    }else if (cel1.textContent =='O'&& cel2.textContent=='O'&& cel3.textContent=='O'){
+        alert('Y won!')
+        cel1.classList.add('win')
+        cel2.classList.add('win')
+        cel3.classList.add('win')
+
+    } else if (cel4.textContent=='X'&& cel5.textContent=='X'&& cel6.textContent=='X'){
+        alert('X won!')
+        cel4.classList.add('win')
+        cel5.classList.add('win')
+        cel6.classList.add('win')
+    } else if (cel4.textContent=='O'&& cel5.textContent=='O'&& cel6.textContent=='O'){
+        alert('Y won!')
+        cel4.classList.add('win')
+        cel5classList.add('win')
+        cel6.classList.add('win')
+
+    } else if (cel7.textContent=='X' && cel8.textContent=='X'&& cel9.textContent=='X'){
+        alert('X won!')
+        cel7.classList.add('win')
+        cel8.classList.add('win')
+        cel9.classList.add('win')
+    } else if (cel7.textContent=='O'&& cel8.textContent=='O'&& cel9.textContent=='O'){
+        alert('Y won!')
+        cel7.classList.add('win')
+        cel8.classList.add('win')
+        cel9.classList.add('win')
+
+    } else if (cel9.textContent=='X'&& cel6.textContent=='X'&& cel3.textContent=='X'){
+        alert('X won!')
+        cel9.classList.add('win')
+        cel6.classList.add('win')
+        cel3.classList.add('win')
+
+    } else if (cel9.textContent=='O'&& cel6.textContent=='O'&& cel3.textContent=='O'){
+        alert('Y won!')
+        cel9.classList.add('win')
+        cel6.classList.add('win')
+        cel3.classList.add('win')
+
+    } else if (cel8.textContent=='X'&& cel5.textContent=='X'&& cel2.textContent=='X'){
+        alert('X won!')
+        cel8.classList.add('win')
+        cel5.classList.add('win')
+        cel2.classList.add('win')
+    } else if (cel8.textContent=='O'&& cel5.textContent=='O'&& cel2.textContent=='O'){
+        alert('Y won!')
+        cel8.classList.add('win')
+        cel5.classList.add('win')
+        cel2.classList.add('win')
+
+    } else if (cel7.textContent=='X'&& cel4.textContent=='X'&& cel1.textContent=='X'){
+        alert('X won!')
+        cel7.classList.add('win')
+        cel4.classList.add('win')
+        cel1.classList.add('win')
+    } else if (cel7.textContent=='O'&& cel4.textContent=='O'&& cel1.textContent=='O'){
+        alert('Y won!')
+        cel7.classList.add('win')
+        cel4.classList.add('win')
+        cel1.classList.add('win')
+    } else if (cel9.textContent=='X'&& cel5.textContent=='X'&& cel1.textContent=='X'){
+        alert('X won!')
+        cel9.classList.add('win')
+        cel5.classList.add('win')
+        cel1.classList.add('win')
+    } else if (cel9.textContent=='O'&& cel5.textContent=='O'&& cel1.textContent=='O'){
+        alert('Y won!')
+        cel9.classList.add('win')
+        cel5.classList.add('win')
+        cel1.classList.add('win')
+
+    } else if (cel7.textContent=='X'&& cel5.textContent=='X'&& cel3.textContent=='X'){
+        alert('X won!')
+        cel7.classList.add('win')
+        cel5.classList.add('win')
+        cel3.classList.add('win')
+
+    } else if (cel7.textContent=='O'&& cel5.textContent=='O'&& cel3.textContent=='O'){
+        alert('Y won!')
+        cel7.classList.add('win')
+        cel5.classList.add('win')
+        cel3.classList.add('win')
+        
+    }
+
+};
 
 
+let countRounds = function(){
+    let rounds = 9
+}
